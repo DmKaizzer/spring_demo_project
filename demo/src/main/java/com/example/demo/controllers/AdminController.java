7ee9d8567cfadfcc44fe7ef8062cae0cfe68e9db
Dmitriy.Yamshchikov <Dmitriy.Yamshchikov@fleetcor.ru>
2023-08-29 19:38:09 +0300

    spring-3

--- demo/src/main/java/com/example/demo/controllers/AdminController.java
+++ demo/src/main/java/com/example/demo/controllers/AdminController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminController {
-        @GetMapping(value = "admin")
+    @GetMapping(value = "admin")
     public String getTestData() {
         return "test admin data";
     }
